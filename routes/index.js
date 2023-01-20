1ef430cada1e71ecaf3d44af321ba74e42952e69
Akash <akashp77055@gmal.com>
2023-01-20 17:58:18 +0530

    commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Aakash Patil" });
+  res.render("index", {
+    title: "Aakash Patil",
+    subtitle: "Welcome to my ",
+  });
 });
 
+router.get("/tutorials", function (req, res, next) {
+  const courseName = req.query.course;
+  let courseTitle = "AP Tutorials - " + courseName;
+  res.render("index", {
+    title: courseTitle,
+    subtitle: "Welcome to my " + courseName + " course",
+  });
+});
 module.exports = router;
